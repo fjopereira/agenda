59f730f935d0a9d86e2caa2b450ec49cb9d8e51f
Fabio Jose Oliveira Pereira <15893@tambasa.local>
2021-06-15 17:38:25 -0300

    última aula

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models import Evento
+from django.contrib.auth.models import User
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
