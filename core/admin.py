3df8a6ee9146424d8a6054f9a053df7e248dc98d
Fabio Jose Oliveira Pereira <15893@tambasa.local>
2021-06-14 22:48:50 -0300

    aula-4 / parte 1

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
-    list_filter = ('titulo', 'data_evento',)
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
+    list_filter = ('id', 'titulo', 'data_evento',)
 
 
 admin.site.register(Evento, EventoAdmin)
